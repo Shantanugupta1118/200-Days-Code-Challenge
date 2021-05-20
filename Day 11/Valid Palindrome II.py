c967e39df2cdf386c628e0d4b60b18252f3248ea
shantanugupta1118 <shantanuguptaknp@gmail.com>
2021-05-20 11:32:43 +0530

    LC-680

--- Day 11/Valid Palindrome II.py
+++ Day 11/Valid Palindrome II.py
@@ -6,7 +6,7 @@
 
 # Solutions
 
-def validate_pal(s, counter=1):
+"""def validate_pal(s, counter=1):
     i, j = 0, len(s)-1
     while i<j and s[i]==s[j]:
         i+=1
@@ -16,6 +16,21 @@ def validate_pal(s, counter=1):
     return validate_pal(s[i+1:j+1], 0) or validate_pal(s[i:j], 0)
 
 
-# string = input()
-string = 'deeee'
+string = input()
 print(validate_pal(string))
+"""
+
+
+class Solution:
+    def validPalindrome(self, s, counter=1) -> bool:
+        i, j = 0, len(s)-1
+        while i<j and s[i]==s[j]:
+            i+=1
+            j-=1
+        if i>=j: return True
+        if counter==0: return False
+        return self.validPalindrome(s[i+1:j+1], 0) or self.validPalindrome(s[i:j], 0)
+
+string = input()
+Lc = Solution()
+print(Lc.validPalindrome(string))
