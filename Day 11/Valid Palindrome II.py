91db9876f670b2192cea4a78cea2f6cbe94d0f0d
shantanugupta1118 <shantanuguptaknp@gmail.com>
2021-05-20 11:29:18 +0530

    LC-680

---
+++ Day 11/Valid Palindrome II.py
@@ -0,0 +1,21 @@
+# Github: Shantanugupta1118
+# DAY 11 of DAY 100
+# 680. Valid Palindrome II
+# https://leetcode.com/problems/valid-palindrome-ii/
+
+
+# Solutions
+
+def validate_pal(s, counter=1):
+    i, j = 0, len(s)-1
+    while i<j and s[i]==s[j]:
+        i+=1
+        j-=1
+    if i>=j: return True
+    if counter==0: return False
+    return validate_pal(s[i+1:j+1], 0) or validate_pal(s[i:j], 0)
+
+
+# string = input()
+string = 'deeee'
+print(validate_pal(string))
