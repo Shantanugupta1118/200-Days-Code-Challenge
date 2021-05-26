1c3816e13b3eedcd92099c392a282116bc99f168
shantanugupta1118 <shantanuguptaknp@gmail.com>
2021-05-26 23:56:10 +0530

    LC

--- Day 17/11. Container With Most Water.py
+++ Day 17/11. Container With Most Water.py
@@ -6,18 +6,31 @@
 
 # solution
 
+# def solve(h):
+#     mx = 0
+#     # mid = len(h)//2
+#     n = len(h)
+#     i = 0
+#     while i!=n-1:
+#         steps = 0
+#         for j in range(i, n):
+#             mx = max(mx, min(h[i], h[j])*steps)
+#             steps += 1
+#         i += 1
+#         #     print("in", mx, steps)
+#         # print(mx, steps)
+#     return mx
+
+
 def solve(h):
     mx = 0
-    # mid = len(h)//2
-    n = len(h)
-    for i in range(n-1 ):
-        steps = 0
-        for j in range(i, n):
-            mx = max(mx, min(h[i], h[j])*steps)
-            steps += 1
-        #     print("in", mx, steps)
-        # print(mx, steps)
+    i, j = 0, len(h)-1
+    while i<j:
+        mx = max(mx, min(h[i], h[j])*(j-i))
+        if h[i] < h[j]: i+=1
+        else: j -= 1
     return mx
+
 
 height = list(map(int, input().split()))
 print(solve(height))
