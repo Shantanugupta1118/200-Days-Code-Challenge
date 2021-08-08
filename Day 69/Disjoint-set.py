292f4b8268af95bc49bdfbeac44163328dd154e4
shantanugupta1118 <shantanuguptaknp@gmail.com>
2021-08-09 00:14:20 +0530

    GeekForGeeks/Practice

--- Day 69/Disjoint-set.py
+++ Day 69/Disjoint-set.py
@@ -1,21 +1,34 @@
 # Github: Shantanugupta1118
 # DAY 69 of DAY 100
+# Disjoint Union - GFG
+# https://practice.geeksforgeeks.org/problems/disjoint-set-union-find/1#
+
 
 class Solution:
-    def solve(self, n, k, queries):
-        arr = {}
-        res = []
-        for i in range(1,n+1):
-            arr[i] = i
-        for query in queries:
-            if query[0] == 'find':
-                res.append(arr[query[1]])
-            else:
-                arr[query[1]] = arr[query[2]]
-        return res
+    def find(self, parent, x):
+        if x == parent[x-1]:
+            return x
+        parent[x-1] = self.find(parent, parent[x-1])
+        return parent[x-1]
+
+    def unionSet(self, parent, x, y):
+        a = self.find(parent, parent[x-1])
+        b = self.find(parent, parent[y-1])
+        if a!=b:
+            parent[a-1] = b
+
 
 for _ in range(int(input())):
-    # N, K = map(int, input().split())
-    N, K = 5, 4
-    query = [['find',4],['find',1],['unionSet',3,1],['find',3]]
-    print(Solution().solve(N, K, query))
+    n, k = map(int, input().split())
+    arr = [x for x in range(1, n+1)]
+    str = input().strip().split()
+    i = 0
+    f = Solution()
+    while i<len(str):
+        if str[i] == 'FIND':
+            print(f.find(arr, int(str[i+1])), end=" ")
+            i += 2 
+        elif str[i] == 'UNION':
+            f.unionSet(arr, int(str[i+1]), int(str[i+2]))
+            i += 3
+    print()
