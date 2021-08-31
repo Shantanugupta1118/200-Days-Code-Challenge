0a22200865092f5a81d9bba845cc6c622a0e7561
shantanugupta1118 <shantanuguptaknp@gmail.com>
2021-08-31 13:46:21 +0530

    LC

--- Day 73/remove duplicates from sorted array.py
+++ Day 73/remove duplicates from sorted array.py
@@ -5,3 +5,20 @@
 
 
 
+class solution:
+    def removeDuplicates(self, nums):
+        if len(nums) is None:
+            return 0
+        if len(nums)==0 or len(nums)==1:
+            return len(nums)
+
+        length_new = len(nums)
+        count = len(nums)-1
+        while count>0:
+            if nums[count] == nums[count-1]:
+                del nums[count]
+                length_new -= 1
+            count -= 1
+        return length_new
+
+print(solution().removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
