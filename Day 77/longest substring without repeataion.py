3eef4e9d74a8e77c6a11d8d7cc7684f186b08dd6
shantanugupta1118 <shantanuguptaknp@gmail.com>
2021-09-06 15:03:33 +0530

    Code

---
+++ Day 77/longest substring without repeataion.py
@@ -0,0 +1,22 @@
+# Github: Shantanugupta1118
+# DAY 65 of DAY 100
+
+class Solution:
+    def solve(self, s):
+        characters = set()
+        left = right = ans = 0
+        length = len(s)
+
+        while right < length:
+            if s[right] in characters:
+                characters.remove(s[left])
+                left += 1
+            else:
+                characters.add(s[right])
+                right += 1
+                ans = max(ans, right - left)
+
+        return ans
+
+s = input()
+print(Solution().solve(s))
