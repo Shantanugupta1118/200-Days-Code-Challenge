e389b077ad08695cfa7e79c071a7c55d7e985962
shantanugupta1118 <shantanuguptaknp@gmail.com>
2021-09-06 14:48:24 +0530

    Code

--- Day 77/Slowest Key.py
+++ Day 77/Slowest Key.py
@@ -2,8 +2,26 @@
 # DAY 77 of DAY 100
 
 class Solution:
-    def solve(self):
-        pass
+    def solve(self, time, keyPress):
+        times = {time[0]: [keyPress[0]]}
+        for i in range(1 , len(time)):
+            t = time[i] - time[i - 1]
+            if(t in times):
+                times[t].append(keyPress[i])
+            else:
+                times[t] = [keyPress[i]]
+        keys = times[max(times.keys())]
+        return max(keys)
+        # diff = time[0]
+        # idx = 0
+        # for i in range(1, len(time)):
+        #     if time[i]-time[i-1] >= diff:
+        #         diff = time[i]-time[i-1]
+        #         idx = i
+        # # print(diff, keyPress[idx])
+        # return keyPress[idx]
 
-for _ in range(int(input())):
-    print(Solution().solve())
+
+release_time = [int(x) for x in input().split()]
+keysPressed = input()
+print(Solution().solve(release_time, keysPressed))
