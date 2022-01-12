5986c3dd72a1f09e7cdfa635f5404d36a20390a6
Shantanu Gupta <shantanuguptaknp@gmail.com>
2022-01-12 22:53:31 +0530

    Leetcode

---
+++ Day 96/701. Insert into a Binary Search Tree.py
@@ -0,0 +1,24 @@
+# Github: Shantanugupta1118
+# DAY 96 of DAY 100
+# 701. Insert into a Binary Search Tree
+
+
+
+# Definition for a binary tree node.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def insertIntoBST(self, root, val):
+        if root==None:
+            return TreeNode(val)
+        if val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+        else:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
+
+print(Solution().solve([4,2,7,1,3]))
