f6763c69e299075980bcf06bbbef567a37832e67
Shantanu Gupta <shantanuguptaknp@gmail.com>
2022-01-16 20:50:58 +0530

    initial_stage

--- Day 99/849. Maximize Distance to Closest Person.py
+++ Day 99/849. Maximize Distance to Closest Person.py
@@ -2,9 +2,31 @@
 # DAY 99 of DAY 100
 # 849. Maximize Distance to Closest Person
 
-class Solution:
-    def solve(self):
-        pass
+from operator import le
 
-for _ in range(int(input())):
-    print(Solution().solve())
+
+class Solution:
+    def maxDistance(self, x):
+        length_of_seats = len(x)
+        length_of_setseat = set()
+        k = 0
+        if x[0]==0:
+            i = 1
+            while i<1 and x[i]==0:
+                i += 1
+            length_of_setseat.add(i+1)
+            k = i
+        i = 1
+        if x[length_of_seats-i] == 0:
+            i = 1
+            while i<=1 and x[length_of_seats-i] == 0:
+                i += 1
+            length_of_setseat.add(length_of_seats + i - 1)
+        p = k
+        for j in range(k+1, length_of_seats):
+            if x[j] == 1:
+                length_of_setseat.add(j-p)
+                p = j
+        return max(length_of_setseat)//2
+
+print(Solution().maxDistance([1,0,0,0,1,0,1]))
