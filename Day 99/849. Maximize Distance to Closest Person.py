55b4ae82ab6badecc6add185d183592e7d086285
Shantanu Gupta <shantanuguptaknp@gmail.com>
2022-01-16 20:42:44 +0530

    initial_stage

---
+++ Day 99/849. Maximize Distance to Closest Person.py
@@ -0,0 +1,10 @@
+# Github: Shantanugupta1118
+# DAY 99 of DAY 100
+# 849. Maximize Distance to Closest Person
+
+class Solution:
+    def solve(self):
+        pass
+
+for _ in range(int(input())):
+    print(Solution().solve())
