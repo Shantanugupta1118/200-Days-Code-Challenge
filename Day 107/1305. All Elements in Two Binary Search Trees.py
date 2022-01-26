a44f4edc26eb161dff604c1f61b5172be323245e
Shantanu Gupta <shantanuguptaknp@gmail.com>
2022-01-26 21:13:42 +0530

    leetcode

--- Day 107/1305. All Elements in Two Binary Search Trees.py
+++ Day 107/1305. All Elements in Two Binary Search Trees.py
@@ -9,6 +9,7 @@ class TreeNode:
         self.left = left
         self.right = right
 
+
 class Solution:
     def getAllElements(self, root1, root2):
         arr1, arr2 = [], []
@@ -25,3 +26,43 @@ class Solution:
             self.inorder(root.right, arr)
         return arr
         
+    def getNode(self, data):
+        newNode = TreeNode(data)
+        newNode.val = data
+        newNode.left = None
+        newNode.right = None
+        return newNode
+
+    def levelOrder(self,root, data):
+        if root == None:
+            root = self.getNode(data)
+            return root
+        if data <= root.val:
+            root.left = self.levelOrder(root.left, data)
+        else:
+            root.right = self.levelOrder(root.right, data)
+        return root
+   
+    def constructBST(self, arr, n):
+        if n == 0: return None
+        root = None
+        for i in range(n):
+            root = self.levelOrder(root, arr[i])
+        return root
+    
+    
+    
+    
+# ---------- MAIN DRIVER -----------
+arr1 = [7,8,4,12,3,6,8,1,5,10]
+n = len(arr1)
+root1 = Solution().constructBST(arr1, n)
+
+arr2 = [9,6,2,4,74,1,2,5]
+n1 = len(arr2)
+root2 = Solution().constructBST(arr2, n1)
+
+root = Solution().getAllElements(root1, root2)
+print("Merged BST: ", root)
+    
+    
