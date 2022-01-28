1fb82ae5897a5f7451e097e94d3f8a5824bce7a0
Shantanu Gupta <shantanuguptaknp@gmail.com>
2022-01-28 21:11:30 +0530

    leetcode

---
+++ Day 108/211. Design Add and Search Words Data Structure.py
@@ -0,0 +1,39 @@
+# Github: Shantanugupta1118
+# DAY 109 of DAY 100
+# 211. Design Add and Search Words Data Structure
+
+
+import re
+
+class Solution:
+    def __init__(self):
+        self.words = []
+    
+    def addWord(self, word) -> None:
+        self.words.append(word)
+        
+    def search(self, word) -> bool:
+        if '.' in word:
+            res = re.findall(word, " ".join(self.words))
+            for i in res:
+                i = i.strip()
+                if len(i) == len(word):
+                    return True
+            return False
+        
+        else:
+            if word in self.words:
+                return True
+            return False
+        
+    
+    
+wordDic = Solution()
+wordDic.addWord("a")
+wordDic.addWord("a")
+print(wordDic.search("."))
+print(wordDic.search("a"))
+print(wordDic.search("aa"))
+print(wordDic.search("a"))
+print(wordDic.search(".a"))
+print(wordDic.search("a."))
