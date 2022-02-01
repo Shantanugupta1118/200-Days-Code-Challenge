eddf12823dd1bc6dc10e1a6c7f8b70c165124bb7
Shantanu Gupta <shantanuguptaknp@gmail.com>
2022-02-01 16:38:37 +0530

    leetcode

--- Day 112/121. Best Time to Buy and Sell Stock.py
+++ Day 112/121. Best Time to Buy and Sell Stock.py
@@ -4,4 +4,19 @@
 
 
 
- 
+class Solution:
+    def maxSell(self, prices):
+        if len(prices)<=1: return 0
+        profit, buy_price = 0, max(prices)
+        for i in prices:
+            buy_price = min(buy_price, i)
+            profit = max(i - buy_price, profit)
+        return profit             
+                 
+         
+         
+         
+         
+print(Solution().maxSell([7,1,5,3,6,4]))
+print(Solution().maxSell([7,6,4,3,1]))
+print(Solution().maxSell([2,4,1]))
