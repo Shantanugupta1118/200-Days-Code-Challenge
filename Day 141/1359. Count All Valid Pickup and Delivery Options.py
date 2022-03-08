5c9ea4a5e8cd236db1a9b5106b2c171d2223f4a6
Shantanu Gupta <shantanuguptaknp@gmail.com>
2022-03-08 15:26:07 +0530

    leetcode

---
+++ Day 141/1359. Count All Valid Pickup and Delivery Options.py
@@ -0,0 +1,16 @@
+# GithubID: Shantanugupta1118
+# Day 141 of 200 days
+# 1359. Count All Valid Pickup and Delivery Options
+
+
+
+class Solution:
+    def countOrder(self, n):
+        if n == 1:
+            return n
+        
+
+
+print(Solution().countOrder(1))
+print(Solution().countOrder(5))
+print(Solution().countOrder(2))
